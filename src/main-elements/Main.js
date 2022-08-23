58eedab1c580fafb22a502e68f6b50ff5eede651
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-08-23 10:03:09 -0700

    start on basic layout of each book card

--- src/main-elements/Main.js
+++ src/main-elements/Main.js
@@ -1,9 +1,21 @@
 import Navigation from "./Navigation";
 
-const Main = () => {
+const Main = ({ savedBooks }) => {
 	return (
 		<main>
-			Main
+			<div className="book-container">
+				{savedBooks.map((book) => (
+					<div className="book-card" key={book.id}>
+						<div>
+							<img src={book.thumbnail} alt="" />
+						</div>
+						<div className="book-right">
+							<h4>{book.title}</h4>
+							<p>{book.author[0]}</p>
+						</div>
+					</div>
+				))}
+			</div>
 			<Navigation />
 		</main>
 	);
