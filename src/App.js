2fe9bac03536fe63f740c3f6cee5cf68a002f710
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-08-23 10:00:29 -0700

    add state for saved books, added a couple starter books to work on styling/functions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import Header from "./main-elements/Header";
 import Main from "./main-elements/Main";
+import FindBookModel from "./components/FindBookModel";
+import React, { useState, useEffect } from "react";
 
 function App() {
+	const [savedBooks, setSavedBooks] = useState([
+		{
+			author: ["J.R.R. Tolkien"],
+			id: "9780547951942",
+			thumbnail:
+				"http://books.google.com/books/content?id=yl4dILkcqm4C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+			title: "The Lord of the Rings",
+		},
+		{
+			author: ["J.R.R. Tolkien"],
+			id: "9780547952017",
+			thumbnail:
+				"http://books.google.com/books/content?id=aWZzLPhY4o0C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+			title: "The Fellowship Of The Ring",
+		},
+	]);
+
+	useEffect(() => {}, []);
+
 	return (
 		<div className="App">
+			<FindBookModel />
 			<Header />
-			<Main />
+			<Main savedBooks={savedBooks} />
 		</div>
 	);
 }
