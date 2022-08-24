ced146f7c2948eec1fcc873e887b8d9ee17f1007
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-08-24 09:35:47 -0700

    add search options for author and books, started to work on filter by read status

--- src/main-elements/Navigation.js
+++ src/main-elements/Navigation.js
@@ -1,5 +1,49 @@
+import React, { useState } from "react";
+
 const Navigation = () => {
-	return <aside>Navigation</aside>;
+	const [readStatusFilter, setReadStatusFilter] = useState("All")
+	const [isReadStatusDropdownOpen, setIsReadStatusDropdownOpen] = useState(false)
+
+	const openReadStatusDropdown = () => {
+		if (isReadStatusDropdownOpen) {
+			setIsReadStatusDropdownOpen(false)
+		} else {
+			setIsReadStatusDropdownOpen(true)
+		}
+	}
+	function updateReadStatusFilter(e) {
+		setReadStatusFilter(e.target.textContent)
+		openReadStatusDropdown();
+	}
+	return (
+		<aside>
+			<label htmlFor="title-search">Search for Title:</label>
+			<input type="search" name="title-search" id="title-search" />
+			<label htmlFor="author-search">Search for Author:</label>
+			<input type="search" name="author-search" id="author-search" />
+			<fieldset>
+			<legend>Filter By:</legend>
+				<fieldset className="read-status-fieldset">
+					<legend className="read-status-legend">Read Status</legend>
+						<button 
+							className="read-status-btn"
+							onClick={openReadStatusDropdown}
+							>{readStatusFilter}<p>^</p>
+						</button>
+						{isReadStatusDropdownOpen ? 
+						<ol className="read-status-dropdown">
+							<li className="read-status-option"
+								onClick={updateReadStatusFilter}>All</li>
+							<li className="read-status-option"
+								onClick={updateReadStatusFilter}>Unread</li>
+							<li className="read-status-option"
+								onClick={updateReadStatusFilter}>Reading</li>
+							<li className="read-status-option"
+								onClick={updateReadStatusFilter}>Read</li>
+						</ol> : null}
+				</fieldset>
+			</fieldset>
+		</aside>);
 };
 
 export default Navigation;
