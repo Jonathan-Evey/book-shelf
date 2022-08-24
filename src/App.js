80c6de7142895341e0e533e4a1ee69863e7b79d6
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-08-24 13:22:35 -0700

    added ratings and updated ids on saved books

--- src/App.js
+++ src/App.js
@@ -7,62 +7,68 @@ function App() {
 	const [savedBooks, setSavedBooks] = useState([
 		{
 			author: ["J.R.R. Tolkien"],
-			id: "9780547951942",
+			id: "1",
 			thumbnail:
 				"http://books.google.com/books/content?id=yl4dILkcqm4C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
 			title: "The Lord of the Rings",
 			readStatus: "Unread",
+			rating: 1,
 			genres: ["Fiction", "Fantasy"],
 			isPrioritized: false,
 		},
 		{
 			author: ["J.R.R. Tolkien"],
-			id: "9780547952017",
+			id: "2",
 			thumbnail:
 				"http://books.google.com/books/content?id=aWZzLPhY4o0C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
 			title: "The Fellowship Of The Ring",
 			readStatus: "Read",
+			rating: 2,
 			genres: ["Fiction"],
 			isPrioritized: false,
 		},
 		{
 			author: ["J.R.R. Tolkien"],
-			id: "9780547951942",
+			id: "3",
 			thumbnail:
 				"http://books.google.com/books/content?id=yl4dILkcqm4C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
 			title: "The Lord of the Rings",
 			readStatus: "Unread",
+			rating: 3,
 			genres: ["Fiction", "Fantasy"],
 			isPrioritized: false,
 		},
 		{
 			author: ["J.R.R. Tolkien"],
-			id: "9780547952017",
+			id: "4",
 			thumbnail:
 				"http://books.google.com/books/content?id=aWZzLPhY4o0C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
 			title: "The Fellowship Of The Ring",
 			readStatus: "Read",
+			rating: 4,
 			genres: ["Fiction", "Fantasy"],
 			isPrioritized: false,
 		},
 		{
 			author: ["J.R.R. Tolkien"],
-			id: "9780547951942",
+			id: "5",
 			thumbnail:
 				"http://books.google.com/books/content?id=yl4dILkcqm4C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
 			title: "The Lord of the Rings",
 			genres: ["Fiction", "Fantasy"],
 			readStatus: "Unread",
+			rating: 5,
 			isPrioritized: false,
 		},
 		{
 			author: ["J.R.R. Tolkien"],
-			id: "9780547952017",
+			id: "6",
 			thumbnail:
 				"http://books.google.com/books/content?id=aWZzLPhY4o0C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
 			title: "The Fellowship Of The Ring",
 			genres: ["Fiction", "Fantasy"],
 			readStatus: "Read",
+			rating: "",
 			isPrioritized: false,
 		},
 	]);
