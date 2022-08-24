4d6d9f1cc56f33c717e42633b1dd53f783a3c6db
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-08-24 13:20:04 -0700

    add a rating to the saved books, and now pull that on display

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -22,7 +22,7 @@ const BookCard = ({ book }) => {
 					<h4>{book.title}</h4>
 					<p>{book.author[0]}</p>
 				</div>
-				<p>Personal Rating: 4.5/5</p>
+				<p>Personal Rating: {book.rating}/5</p>
 				<div className="book-right-bottom-container">
 					{book.readStatus === "Read" ? null : (
 						<button className="prioritize-btn">Prioritize</button>
