613f1074e76526787c28329e55466bc19bcfd995
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-08-24 09:33:52 -0700

    added more books to test out display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import Header from "./main-elements/Header";
 import Main from "./main-elements/Main";
+import Header from "./main-elements/Header";
 import FindBookModel from "./components/FindBookModel";
 import React, { useState, useEffect } from "react";
 
 function App() {
 	const [savedBooks, setSavedBooks] = useState([
+		{
+			author: ["J.R.R. Tolkien"],
+			id: "9780547951942",
+			thumbnail:
+				"http://books.google.com/books/content?id=yl4dILkcqm4C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+			title: "The Lord of the Rings",
+			readStatus: "Unread",
+			isPrioritized: false,
+		},
+		{
+			author: ["J.R.R. Tolkien"],
+			id: "9780547952017",
+			thumbnail:
+				"http://books.google.com/books/content?id=aWZzLPhY4o0C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+			title: "The Fellowship Of The Ring",
+			readStatus: "Read",
+			isPrioritized: false,
+		},
+		{
+			author: ["J.R.R. Tolkien"],
+			id: "9780547951942",
+			thumbnail:
+				"http://books.google.com/books/content?id=yl4dILkcqm4C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+			title: "The Lord of the Rings",
+			readStatus: "Unread",
+			isPrioritized: false,
+		},
+		{
+			author: ["J.R.R. Tolkien"],
+			id: "9780547952017",
+			thumbnail:
+				"http://books.google.com/books/content?id=aWZzLPhY4o0C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+			title: "The Fellowship Of The Ring",
+			readStatus: "Read",
+			isPrioritized: false,
+		},
 		{
 			author: ["J.R.R. Tolkien"],
 			id: "9780547951942",
