56154760628223aebdc3d532d45998ccc952ef92
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-08-24 06:58:59 -0700

    updated to pass through book data

--- src/main-elements/Main.js
+++ src/main-elements/Main.js
@@ -1,29 +1,10 @@
+import BookContainer from "./BookContainer";
 import Navigation from "./Navigation";
 
 const Main = ({ savedBooks }) => {
 	return (
 		<main>
-			<div className="book-container">
-				{savedBooks.map((book) => (
-					<div className="book-card" key={book.id}>
-						<div className="book-cover-img">
-							<img src={book.thumbnail} alt="" />
-							<p>Read</p>
-						</div>
-						<div className="book-right">
-							<div className="book-right-top">
-								<h4>{book.title}</h4>
-								<p>{book.author[0]}</p>
-							</div>
-							<div className="book-right-bottom">
-								<button>Own</button>
-								<p>Personal Rating: 4.5/5</p>
-								<p>fantasy</p>
-							</div>
-						</div>
-					</div>
-				))}
-			</div>
+			<BookContainer savedBooks={savedBooks} />
 			<Navigation />
 		</main>
 	);
