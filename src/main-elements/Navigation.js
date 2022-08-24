d098a9260da053419cdb203c7b3fbe18a3707a8e
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-08-24 11:39:55 -0700

    add a rating dropdown to sort books

--- src/main-elements/Navigation.js
+++ src/main-elements/Navigation.js
@@ -4,6 +4,9 @@ const Navigation = () => {
 	const [readStatusFilter, setReadStatusFilter] = useState("All")
 	const [isReadStatusDropdownOpen, setIsReadStatusDropdownOpen] = useState(false)
 
+	const [ratingFilter, setRatingFilter] = useState('All')
+	const [isRatingFilterDropdownOpen, setIsRatingFilterDropdownOpen] = useState(false)
+
 	const openReadStatusDropdown = () => {
 		if (isReadStatusDropdownOpen) {
 			setIsReadStatusDropdownOpen(false)
@@ -15,31 +18,71 @@ const Navigation = () => {
 		setReadStatusFilter(e.target.textContent)
 		openReadStatusDropdown();
 	}
+
+	const openRatingFilterDropdown = () => {
+		if (isRatingFilterDropdownOpen) {
+			setIsRatingFilterDropdownOpen(false)
+		} else {
+			setIsRatingFilterDropdownOpen(true)
+		}
+	}
+
+	function updateRatingFilter(e) {
+		setRatingFilter(e.target.textContent)
+		openRatingFilterDropdown();
+	}
 	return (
 		<aside>
 			<label htmlFor="title-search">Search for Title:</label>
 			<input type="search" name="title-search" id="title-search" />
 			<label htmlFor="author-search">Search for Author:</label>
 			<input type="search" name="author-search" id="author-search" />
-			<fieldset>
-			<legend>Filter By:</legend>
-				<fieldset className="read-status-fieldset">
-					<legend className="read-status-legend">Read Status</legend>
+			<fieldset className="filter-container-fieldset">
+			<legend className="filter-container-legend">Filter By:</legend>
+				<fieldset className="filter-option-fieldset">
+					<legend className="filter-option-legend">Read Status</legend>
 						<button 
-							className="read-status-btn"
+							className="filter-option-btn"
 							onClick={openReadStatusDropdown}
 							>{readStatusFilter}<p>^</p>
 						</button>
 						{isReadStatusDropdownOpen ? 
-						<ol className="read-status-dropdown">
-							<li className="read-status-option"
+						<ol className="filter-option-dropdown">
+							<li className="filter-option"
 								onClick={updateReadStatusFilter}>All</li>
-							<li className="read-status-option"
+							<li className="filter-option"
 								onClick={updateReadStatusFilter}>Unread</li>
-							<li className="read-status-option"
+							<li className="filter-option"
 								onClick={updateReadStatusFilter}>Reading</li>
-							<li className="read-status-option"
+							<li className="filter-option"
 								onClick={updateReadStatusFilter}>Read</li>
+						</ol> : null}
+				</fieldset>
+				<fieldset className="filter-option-fieldset">
+					<legend className="filter-option-legend">Rating:</legend>
+						<button 
+							className="filter-option-btn"
+							onClick={openRatingFilterDropdown}
+							>{ratingFilter}<p>^</p>
+						</button>
+						{isRatingFilterDropdownOpen ? 
+						<ol className="filter-option-dropdown">
+							<li className="filter-option"
+								onClick={updateRatingFilter}>All</li>
+							<li className="filter-option"
+								onClick={updateRatingFilter}>No Rating</li>
+							<li className="filter-option"
+								onClick={updateRatingFilter}>Ratings 0 - 1</li>
+							<li className="filter-option"
+								onClick={updateRatingFilter}>Ratings 1 - 2</li>
+							<li className="filter-option"
+								onClick={updateRatingFilter}>Ratings 2 - 3</li>
+							<li className="filter-option"
+								onClick={updateRatingFilter}>Ratings 3 - 4</li>
+							<li className="filter-option"
+								onClick={updateRatingFilter}>Ratings 4 - 5</li>
+							<li className="filter-option"
+								onClick={updateRatingFilter}>Ratings 5</li>
 						</ol> : null}
 				</fieldset>
 			</fieldset>
