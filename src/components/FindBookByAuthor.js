f72b86091c19651b6c5479c4a92ce1812ceada9d
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-09-12 18:39:24 -0700

    add enter button capability to the search book inputs

--- src/components/FindBookByAuthor.js
+++ src/components/FindBookByAuthor.js
@@ -1,29 +1,32 @@
 import React from "react";
 
 const FindBookByAuthor = (props) => {
-	const updateAuthorKeyWordEvent = (e) => {
-		props.updateAuthorKeyWord(e.target.value);
-	};
+  const updateAuthorKeyWordEvent = (e) => {
+    props.updateAuthorKeyWord(e.target.value);
+  };
 
-	return (
-		<div className="new-book-search-card">
-			<input
-				type="search"
-				name="new-book-search"
-				id="new-book-search"
-				onChange={updateAuthorKeyWordEvent}
-				placeholder="Find by Author"
-				value={props.searchAuthorKeyWord}
-			/>
-			<button
-				onClick={() =>
-					props.openFindBookModel(props.searchAuthorKeyWord)
-				}
-			>
-				Find Book
-			</button>
-		</div>
-	);
+  return (
+    <div className="new-book-search-card">
+      <input
+        type="search"
+        name="new-book-search"
+        id="new-book-search"
+        onChange={updateAuthorKeyWordEvent}
+        onKeyUp={(e) =>
+          e.key === "Enter"
+            ? props.openFindBookModel(props.searchAuthorKeyWord)
+            : false
+        }
+        placeholder="Find by Author"
+        value={props.searchAuthorKeyWord}
+      />
+      <button
+        onClick={() => props.openFindBookModel(props.searchAuthorKeyWord)}
+      >
+        Find Book
+      </button>
+    </div>
+  );
 };
 
 export default FindBookByAuthor;
--- src/components/FindBookByTitle.js
+++ src/components/FindBookByTitle.js
@@ -1,29 +1,30 @@
 import React from "react";
 
 const FindBookByTitle = (props) => {
-	const updateTitleKeyWordEvent = (e) => {
-		props.updateTitleKeyWord(e.target.value);
-	};
+  const updateTitleKeyWordEvent = (e) => {
+    props.updateTitleKeyWord(e.target.value);
+  };
 
-	return (
-		<div className="new-book-search-card">
-			<input
-				type="search"
-				name="new-book-search"
-				id="new-book-search"
-				onChange={updateTitleKeyWordEvent}
-				placeholder="Find by Title"
-				value={props.searchTitleKeyWord}
-			/>
-			<button
-				onClick={() =>
-					props.openFindBookModel(props.searchTitleKeyWord)
-				}
-			>
-				Find Book
-			</button>
-		</div>
-	);
+  return (
+    <div className="new-book-search-card">
+      <input
+        type="search"
+        name="new-book-search"
+        id="new-book-search"
+        onChange={updateTitleKeyWordEvent}
+        onKeyUp={(e) =>
+          e.key === "Enter"
+            ? props.openFindBookModel(props.searchTitleKeyWord)
+            : false
+        }
+        placeholder="Find by Title"
+        value={props.searchTitleKeyWord}
+      />
+      <button onClick={() => props.openFindBookModel(props.searchTitleKeyWord)}>
+        Find Book
+      </button>
+    </div>
+  );
 };
 
 export default FindBookByTitle;
