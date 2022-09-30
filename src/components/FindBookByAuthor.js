cd53c7367c004ef3fcae3f3b3e92b47b818648ff
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-09-30 09:10:07 -0700

    small style addtions mad to buttons

--- src/components/FindBookByAuthor.js
+++ src/components/FindBookByAuthor.js
@@ -1,32 +1,35 @@
 import React from "react";
 
 const FindBookByAuthor = (props) => {
-  const updateAuthorKeyWordEvent = (e) => {
-    props.updateAuthorKeyWord(e.target.value);
-  };
+	const updateAuthorKeyWordEvent = (e) => {
+		props.updateAuthorKeyWord(e.target.value);
+	};
 
-  return (
-    <div className="new-book-search-card">
-      <input
-        type="search"
-        name="new-book-search"
-        id="new-book-search"
-        onChange={updateAuthorKeyWordEvent}
-        onKeyUp={(e) =>
-          e.key === "Enter"
-            ? props.openFindBookModel(props.searchAuthorKeyWord)
-            : false
-        }
-        placeholder="Find by Author"
-        value={props.searchAuthorKeyWord}
-      />
-      <button
-        onClick={() => props.openFindBookModel(props.searchAuthorKeyWord)}
-      >
-        Find Book
-      </button>
-    </div>
-  );
+	return (
+		<div className="new-book-search-card">
+			<input
+				type="search"
+				name="new-book-search"
+				id="new-book-search"
+				onChange={updateAuthorKeyWordEvent}
+				onKeyUp={(e) =>
+					e.key === "Enter"
+						? props.openFindBookModel(props.searchAuthorKeyWord)
+						: false
+				}
+				placeholder="Find by Author"
+				value={props.searchAuthorKeyWord}
+			/>
+			<button
+				className="find-book-btn"
+				onClick={() =>
+					props.openFindBookModel(props.searchAuthorKeyWord)
+				}
+			>
+				Search for Book
+			</button>
+		</div>
+	);
 };
 
 export default FindBookByAuthor;
