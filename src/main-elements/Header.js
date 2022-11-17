5b7707377cbc59b046fb2a2534ea6b84ecb17a3c
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-11-17 14:40:04 -0800

    update to class names

--- src/main-elements/Header.js
+++ src/main-elements/Header.js
@@ -1,12 +1,11 @@
 const Header = () => {
 	return (
-		<header className="box-shadow margin-block-end-8 flex">
+		<header className="box-shadow margin-block-end-24 flex">
 			<div className="title-container">
 				<h1 className="title-main | margin-inline-start-32 fs-900">
 					My Bookshelf
 				</h1>
 			</div>
-			{/* <button>User Menu</button> */}
 		</header>
 	);
 };
