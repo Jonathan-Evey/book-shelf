311a7f6857a24cadfe0a9ce603154eb8ad6d5fe7
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-11-22 11:27:30 -0800

    moved repeat code from menu to input component

--- src/components/findBooks/FindBookInput.js
+++ src/components/findBooks/FindBookInput.js
@@ -6,31 +6,36 @@ const FindBookInput = (props) => {
 	};
 
 	return (
-		<div className="input-btn-group | padding-inline-8">
-			<input
-				className="input controls"
-				type="search"
-				name="new-book-search"
-				id="new-book-search"
-				onChange={handleOnChangeEvent}
-				onKeyUp={(e) =>
-					e.key === "Enter"
-						? props.openFindBookModel(props.searchKeyWordProp)
-						: false
-				}
-				placeholder={props.placeholderProp}
-				value={props.searchKeyWordProp}
-			/>
-			<button
-				className="btn find-book | margin-block-8"
-				onClick={(e) => {
-					props.openFindBookModel(props.searchKeyWordProp);
-					e.target.blur();
-				}}
-			>
-				Search
-			</button>
-		</div>
+		<li className="input-group | top-shadow-light padding-block-end-16">
+			<div className="input-btn-group | padding-inline-8">
+				<label className="margin-block-4" htmlFor={props.inputIdProp}>
+					{props.labelTextProp}
+				</label>
+				<input
+					className="input controls"
+					type="search"
+					name={props.inputIdProp}
+					id={props.inputIdProp}
+					onChange={handleOnChangeEvent}
+					onKeyUp={(e) =>
+						e.key === "Enter"
+							? props.openFindBookModel(props.searchKeyWordProp)
+							: false
+					}
+					placeholder={props.placeholderProp}
+					value={props.searchKeyWordProp}
+				/>
+				<button
+					className="btn find-book | margin-block-8"
+					onClick={(e) => {
+						props.openFindBookModel(props.searchKeyWordProp);
+						e.target.blur();
+					}}
+				>
+					Search
+				</button>
+			</div>
+		</li>
 	);
 };
 
--- src/components/findBooks/FindBookMenu.js
+++ src/components/findBooks/FindBookMenu.js
@@ -2,7 +2,6 @@ import React from "react";
 import FindBookInput from "./FindBookInput";
 
 const FindBookMenu = (props) => {
-	////---find new books
 	function openFindBookModel(keyWord) {
 		props.updateCurrentSearchPageNumber(0);
 		if (keyWord === props.searchTitleKeyWord) {
@@ -23,25 +22,22 @@ const FindBookMenu = (props) => {
 
 	return (
 		<>
-			<div className="input-group | top-shadow margin-block-start-16">
-				<p className="title">By Title:</p>
-
-				<FindBookInput
-					placeholderProp={"Find by title"}
-					onChangeProp={props.updateTitleKeyWord}
-					searchKeyWordProp={props.searchTitleKeyWord}
-					openFindBookModel={openFindBookModel}
-				/>
-			</div>
-			<div className="input-group | top-shadow margin-block-start-16">
-				<p className="title">By Author:</p>
-				<FindBookInput
-					placeholderProp={"Find by author"}
-					onChangeProp={props.updateAuthorKeyWord}
-					searchKeyWordProp={props.searchAuthorKeyWord}
-					openFindBookModel={openFindBookModel}
-				/>
-			</div>
+			<FindBookInput
+				labelTextProp={"By Title:"}
+				inputIdProp={"new-book-search-title"}
+				placeholderProp={"Find by title"}
+				onChangeProp={props.updateTitleKeyWord}
+				searchKeyWordProp={props.searchTitleKeyWord}
+				openFindBookModel={openFindBookModel}
+			/>
+			<FindBookInput
+				labelTextProp={"By Author:"}
+				inputIdProp={"new-book-search-author"}
+				placeholderProp={"Find by author"}
+				onChangeProp={props.updateAuthorKeyWord}
+				searchKeyWordProp={props.searchAuthorKeyWord}
+				openFindBookModel={openFindBookModel}
+			/>
 		</>
 	);
 };
