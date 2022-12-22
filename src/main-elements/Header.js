395949792a80237202a28128272a0696568ebcd2
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-12-22 12:05:48 -0800

    adds signout btn/function still needs styling

--- src/main-elements/Header.js
+++ src/main-elements/Header.js
@@ -1,10 +1,33 @@
-const Header = () => {
+import { auth } from "../firebase";
+import { signOut } from "firebase/auth";
+
+const Header = (props) => {
+	const handleSignout = () => {
+		if (props.user.uid === "guest") {
+			props.setUser(null);
+		} else {
+			signOut(auth)
+				.then(() => {})
+				.catch((error) => {
+					console.log(error);
+				});
+		}
+	};
 	return (
 		<header className="main-header | box-shadow flex">
 			<div className="title-container">
 				<h1 className="title-main | margin-inline-start-32 fs-900">
 					My Bookshelf
 				</h1>
+			</div>
+			<div>
+				<button
+					onClick={() => {
+						handleSignout();
+					}}
+				>
+					sign out
+				</button>
 			</div>
 		</header>
 	);
